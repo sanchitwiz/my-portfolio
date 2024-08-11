106de9933cefab0653fb76f27f7caf0b8aabb6dd
Sanchit Vohra <sanchitvohra2005@gmail.com>
2024-08-12 00:16:33 +0530

    new changes in App.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ import {ComputersCanvas} from "./canvas";
 
 function Hero() {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[80px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+    <section className='relative w-full min-h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[80px] max-w-7xl mx-auto flex flex-col items-start gap-5 sm:flex-row`}>
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
           <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
@@ -16,7 +16,7 @@ function Hero() {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I Make Useful Web Applications for <br className='sm:block hidden' /> the Humanity</p>
         </div>
       </div>
-      <ComputersCanvas />
+      <ComputersCanvas style={{ backgroundColor: 'red', border: '2px solid blue' }} />
 
       <div className='absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center '>
         <a href='#about'>
