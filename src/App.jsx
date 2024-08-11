dc5690292a13446e2bc26e712b86272b9d9b744e
Sanchit Vohra <sanchitvohra2005@gmail.com>
2024-08-12 00:45:29 +0530

    Repairs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Tech from "./components/Tech";
-import Works from "./components/Works";
-import Contact from "./components/Contact";
-import StarsCanvas from "./components/Stars";
-import { useEffect } from "react";
-import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    if (window.location.hash) {
-      window.location.replace('/');
-    }
-  }, []);
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
+const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-[#001319]">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
@@ -29,11 +14,11 @@ function App() {
         <Experience />
         <Tech />
         <Works />
-        <div className="relative z-0">
+        <Feedbacks />
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
-        <Footer />
       </div>
     </BrowserRouter>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
 import { motion } from "framer-motion";
-import { styles } from "../style";
-import {ComputersCanvas} from "./canvas";
 
-function Hero() {
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
   return (
-    <section className='relative w-full min-h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[80px] max-w-7xl mx-auto flex flex-col items-start gap-5 sm:flex-row`}>
-        <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
-          <div className="w-1 sm:h-80 h-40 violet-gradient"></div>
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[70px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
+
         <div>
-          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915edd]'>Sanchit</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I Make Useful Web Applications for <br className='sm:block hidden' /> the Humanity</p>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Sanchit</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop Web Applications <br className='sm:block hidden' />
+            for the Humanity
+          </p>
         </div>
       </div>
-      <ComputersCanvas style={{ backgroundColor: 'red', border: '2px solid blue' }} />
 
-      <div className='absolute xs:bottom-10 bottom-20 w-full flex justify-center items-center '>
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className="w-[25px] h-[44px] rounded-3xl border-4 border-secondary flex justify-center items-start p-1 ">
-            <motion.div 
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
               animate={{
-                y: [0, 18, 0]
+                y: [0, 24, 0],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
-                repeatType: 'loop'
+                repeatType: "loop",
               }}
-              className="w-2 h-2 rounded-full bg-secondary mb-1"
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
         </a>
       </div>
     </section>
   );
-}
+};
 
 export default Hero;
