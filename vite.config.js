de57ec9d87e723a2049de98d7487fbc706bee11c
Sanchit Vohra <sanchitvohra2005@gmail.com>
2024-08-19 14:41:46 +0530

    editted base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/', // Replace <repository-name> with your GitHub repository name
+  base: '/', // Base URL for your custom domain
 });
